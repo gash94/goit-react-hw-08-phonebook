fb82dd511196a6fbe5d03dc1f903511f03286099
gash94 <104354207+gash94@users.noreply.github.com>
2023-04-21 19:56:30 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,9 @@
-// import React, { useState, useEffect } from "react";
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 
 import css from "./App.module.css";
 const App = () => {
-    // const [contacts, setContacts] = useState(
-    //     JSON.parse(localStorage.getItem("contacts")) ?? []
-    // );
-    // const [filter, setFilter] = useState("");
-
-    // useEffect(() => {
-    //     localStorage.setItem("contacts", JSON.stringify(contacts));
-    // }, [contacts]);
-
     return (
         <div className={css.app}>
             <h1>Phonebook</h1>
